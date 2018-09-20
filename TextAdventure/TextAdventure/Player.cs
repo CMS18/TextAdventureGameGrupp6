a332a906017d914eae01cbb570cdfa6e842b5907
Robert Tadjer <robert.tadjer@yh.nackademin.se>
2018-09-20 14:43:36 +0200

    Added base for Text Adventure Game.

---
+++ TextAdventure/TextAdventure/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    class Player
+    {
+            // Namn, Inventory List, Current position
+            // Drop Item, Pickup item, show inventory
+    }
+}
---
+++ TextAdventure/TextAdventure/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    class Room
+    {
+        // Beskrivningar, Room Inventory
+        // Look, Inspect
+    }
+}

8326ef4b118d9a68ece4e65d85f5700450dce5bb
Gabriel Jealmo <gabrieljealmo@gmail.com>
2018-09-21 11:09:54 +0200

    Added a CreateItem method.

--- TextAdventure/TextAdventure/WorldBuilder.cs
+++ TextAdventure/TextAdventure/WorldBuilder.cs
@@ -24,6 +24,11 @@ namespace TextAdventure
             items.Add("GAMEGUIDE");
             CreateRoom( "START", "You are standing in a hallway. Surrounded by doorways all leading to different rooms.", items );
 
+            items.Clear();
+            items.Add("KEY");
+            items.Add("SWORD");
+            CreateRoom("NORTH", "To the north of the house, around the room you see a table and a door.", items);
+
             /*Room westRoom = new Room();
             startRoom.roomDescription = "";
             startRoom.roomInventory.Add("", itemCollection[""]);
@@ -49,46 +54,29 @@ namespace TextAdventure
         {
             Room room = new Room();
             room.roomDescription = "";
+            for (int i = 0; i < items.Count; i++)
+            {
+                room.roomInventory.Add(items[i], itemCollection[items[i]]);
+            }
             roomCollection.Add(name, room);
         }
 
         public void CreateItems()
         {
-            Item item = new Item();
-            item.name = "";
-            item.playerInventoryDesc.Add(item.name, "");
-            item.roomInvectoryDesc.Add(item.name, "");
-            itemCollection.Add(item.name, item);
+            CreateItem("CANDYBAR", "It looks tasty.", "There's a candybar lying all alone on the floor.");
+            CreateItem("GAMEGUIDE", "Valid commands are Go, Use, Take, Drop, Inspect and Look.", "An old and dusty guide for some game long forgotten is lying here.");
 
-            item = new Item();
-            item.name = "";
-            item.playerInventoryDesc.Add(item.name, "");
-            item.roomInvectoryDesc.Add(item.name, "");
-            itemCollection.Add(item.name, item);
-
-            item = new Item();
-            item.name = "";
-            item.playerInventoryDesc.Add(item.name, "");
-            item.roomInvectoryDesc.Add(item.name, "");
-            itemCollection.Add(item.name, item);
-
-            item = new Item();
-            item.name = "";
-            item.playerInventoryDesc.Add(item.name, "");
-            item.roomInvectoryDesc.Add(item.name, "");
-            itemCollection.Add(item.name, item);
-
-            item = new Item();
-            item.name = "";
-            item.playerInventoryDesc.Add(item.name, "");
-            item.roomInvectoryDesc.Add(item.name, "");
-            itemCollection.Add(item.name, item);
+            
 
         }
 
-        public void CreateItem()
+        public void CreateItem(string name, string invDesc, string roomInvDesc)
         {
-
+            Item item = new Item();
+            item.name = name;
+            item.playerInventoryDesc.Add(item.name, invDesc);
+            item.roomInvectoryDesc.Add(item.name, roomInvDesc);
+            itemCollection.Add(item.name, item);
         }
 
     }

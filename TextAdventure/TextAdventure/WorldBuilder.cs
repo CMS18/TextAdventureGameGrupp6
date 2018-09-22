669aa1cf9fdb21a3fc6a70b22be045a38a8fd7c8
Maximilian <max-dz@hotmail.com>
2018-09-22 15:34:34 +0200

    Fixed spelling in descriptions

--- TextAdventure/TextAdventure/WorldBuilder.cs
+++ TextAdventure/TextAdventure/WorldBuilder.cs
@@ -40,20 +40,20 @@ namespace TextAdventure
 
             items.Clear();
             items.Add("TOOTHBRUSH");
-            items.Add("ORANGEJUICE");
+            items.Add("GLASS");
             CreateRoom("WEST", "You entered the western room. Clint Eastwood has gone mental and is " +
                                "lying in bed watching Teletubbies. ", items);
 
             items.Clear();
-            items.Add("LÖSSNUS");
+            items.Add("CONTAINER");
             items.Add("RAT");
             CreateRoom("EAST", "You entered the eastern room. Is that??!! Oh shit, Chuck Norris is here, " +
                                 "trying to build a rocket with a chewing gum and a pencil. Classic Chuck. ", items);
 
-            items.Clear();
-            items.Add("");
-            items.Add("");
-            CreateRoom("EXIT", "Room DESC", items);
+            //items.Clear();
+            //items.Add("");
+            //items.Add("");
+            //CreateRoom("EXIT", "Room DESC", items);
 
             return roomCollection;
 
@@ -75,14 +75,14 @@ namespace TextAdventure
             CreateItem("CANDYBAR", "It looks tasty.", "There's a candybar lying all alone on the floor.");
             CreateItem("GAMEGUIDE", "Valid commands are Go, Use, Take, Drop, Inspect and Look.", 
                 "An old and dusty guide for some game long forgotten is lying here.");
-            CreateItem("KEY", "A golden key", "A small golden object is radiating in the dark.");
+            CreateItem("KEY", "A golden key.", "A small golden object is radiating in the dark.");
             CreateItem("SWORD", "Sharp sword.", "A sword is stuck on the ground.");
-            CreateItem("DAGGER", "A rusty old dagger", "There's a rusty old dagger lying here");
-            CreateItem("DEODORANT", "If you smell shit, use it", "A deodorant is lying just beneath your feet");
+            CreateItem("DAGGER", "A rusty old dagger.", "There's a rusty old dagger lying here.");
+            CreateItem("DEODORANT", "If you smell shit, use it.", "A deodorant is lying just beneath your feet.");
             CreateItem("TOOTHBRUSH", "A used toothbrush, barely any straws are left on it.", "Old and used toothbrush.");
             CreateItem("GLASS", "Some kind of bewerage with orange color.", "Glass containing some kind of bewerage.");
-            CreateItem("LÖSSNUSS", "A container with a text Prima Fint", "A round box of some sort");
-            CreateItem("RAT", "Oh shit, it was dead", "A disgusting albino rat with human eyes");
+            CreateItem("CONTAINER", "A container with a text Prima Fint.", "A round box of some sort.");
+            CreateItem("RAT", "Oh shit, it was dead.", "A disgusting albino rat with no eyes.");
             return itemCollection;
             
         }

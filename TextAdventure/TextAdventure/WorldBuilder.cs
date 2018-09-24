e51cea105c9d660a79b9265853c41256d9dd8f30
Gabriel Jealmo <gabrieljealmo@gmail.com>
2018-09-24 17:35:13 +0200

    Added error handling for use.

--- TextAdventure/TextAdventure/WorldBuilder.cs
+++ TextAdventure/TextAdventure/WorldBuilder.cs
@@ -70,20 +70,20 @@ namespace TextAdventure
 
         public Dictionary<string, Item> CreateItems()
         {
-            CreateItem("CANDYBAR", "It looks tasty.", "There's a candybar lying all alone on the floor.", "0002", true);
+            CreateItem("CANDYBAR", "It looks tasty.", "There's a candybar lying all alone on the floor. ", "0002", true);
             CreateItem("GAMEGUIDE", "Valid commands are: \r\nGo <direction>,\r\nUse <item>, Use <item> on <item>,\r\nTake <item>,\r\nDrop <item>," +
                 "\r\nInspect <item> \r\nand Look.", 
-                "An old and dusty gameguide for some game long forgotten is lying here.", "08", true);
-            CreateItem("GIRL", "", "She is standing completely still in the right corner. You can hear her singing something..\n", "930", false);
-            CreateItem("DEODORANT", "If you smell shit, use it.", "A deodorant is lying just beneath your feet.", "930", true);
-            CreateItem("SKÅNING", "", "As crazy as they come, this Skåning is batshit crazy. I wonder why?\n", "8008", false);
-            CreateItem("TOOTHBRUSH", "A used toothbrush, barely any straws are left on it.", "Old and used toothbrush.", "1001", true);
-            CreateItem("CLINT", "", "Clint gives you a quick stare before turning his head back to the TV.\n", "1009" , false);
+                "An old and dusty gameguide for some game long forgotten is lying here. ", "08", true);
+            CreateItem("GIRL", "", "She is standing completely still in the right corner. You can hear her singing something..\r\n", "930", false);
+            CreateItem("DEODORANT", "If you smell shit, use it.", "A deodorant is lying just beneath your feet. ", "930", true);
+            CreateItem("SKÅNING", "", "As crazy as they come, this Skåning is batshit crazy. I wonder why?\r\n", "8008", false);
+            CreateItem("TOOTHBRUSH", "A used toothbrush, barely any straws are left on it.", "Old and used toothbrush. ", "1001", true);
+            CreateItem("CLINT", "", "Clint gives you a quick stare before turning his head back to the TV.\r\n", "1009" , false);
             CreateItem("POSTER", "A movie poster, Clint Eastwood is the star of the movie.", "A poster hanging on the left side of the room. The good, the bad and the uglys movie cover is printed on it. ", "1000", true);
-            CreateItem("CONTAINER", "A container with a text Prima Fint Snus.", "A round container with some text written on it.", "8008", true);
-            CreateItem("RAT", "", "A disgusting albino rat with human eyes is dashing around the room.", "05", false);
-            CreateItem("CHUCK", "", "You suddenly feel alot better knowing that Chuck is here to save the day. But what is he doing here?\n", "1001" , false);
-            CreateItem("DOOR", "", "There's a sturdy looking door here, it seems locked. I can hear birds chirping on the other side of it.", "1230", false);
+            CreateItem("CONTAINER", "A container with a text Prima Fint Snus.", "A round container with some text written on it. It's lying upside down. ", "8008", true);
+            CreateItem("RAT", "", "A disgusting albino rat with human eyes is dashing around the room. ", "05", false);
+            CreateItem("CHUCK", "", "You suddenly feel alot better knowing that Chuck is here to save the day. But what is he doing here?\r\n", "1001" , false);
+            CreateItem("DOOR", "", "There's a sturdy looking door here, it seems locked. I can hear birds chirping on the other side of it. ", "1230", false);
             return itemCollection;
             
         }
